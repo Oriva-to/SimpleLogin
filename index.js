3fd13043c2e92b2604572cf1c92e9c83cd9e557b
Oriva-to <oliverM-M@hotmail.com>
2025-11-30 18:57:24 -0400

    Todo listo

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
-let principal = document.getElementById("primero");
-let secundario = document.getElementById("segundo");
+let crearCuenta = document.getElementById("crear_cuenta");
+let iniciarSesion = document.getElementById("iniciar_sesion");
+let otraPagina = document.getElementById("otra");
+let formulario = document.getElementsByClassName("formulario")[0];
+/* Datos de iniciar sesion*/
+let usuarioIniciarSesion = document.getElementById("usuario_iniciar_sesion");
+let contrasenaIniciarSesion = document.getElementById("contrasena_iniciar_sesion");
 
-principal.style.display = "block";
+/* Datos de creacion de la cuenta */
+let usuarioCrearCuenta = document.getElementById("usuario_crear_cuenta");
+let emailCrearCuenta = document.getElementById("email_crear_cuenta");
+let contrasenaCrearCuenta = document.getElementById("contrasena_crear_cuenta");
 
-if(principal.style.display === "block"){
-    secundario.style.display = "none";
-}
-if(secundario.style.display === "none"){
-    principal.style.display = "block";
-}
+iniciarSesion.style.display = "block";
+crearCuenta.style.display = "none";
+otraPagina.style.display = "none";
 
-function Siguiente(){
-    principal.style.display = "none";
-    secundario.style.display = "block";
+//#region Funciones para cambiar de pagina
+function paginaCrearCuenta(){
+    crearCuenta.style.display = "block";
+    iniciarSesion.style.display = "none";
 }
-function Principal(){
-    secundario.style.display = "none";
-    principal.style.display = "block";
+function PaginaIniciarSesion(){
+    crearCuenta.style.display = "none";
+    iniciarSesion.style.display = "block";
+}
+//#endregion
+let cuenta = {NombreUsuario: null, Email: null, Contrasena: null};
+
+function IniciarSesion(){
+    if(usuarioIniciarSesion.value === cuenta.NombreUsuario && contrasenaIniciarSesion.value === cuenta.Contrasena){
+        alert("Inicio de sesion exitoso");
+        iniciarSesion.style.display = "none";
+        otraPagina.style.display = "block";
+        formulario.style.display = "none";
+    }
+    if(usuarioIniciarSesion.value.trim() === "" && contrasenaIniciarSesion.value.trim() === ""){
+        alert("Los datos estan vacios, huevon :p");
+        return;
+    }
+    if(usuarioIniciarSesion.value.trim() === "" || contrasenaIniciarSesion.value.trim() === ""){
+        alert("hay datos vacios, huevon :p");
+        return;
+    }
+    else{
+        calert("Usuario o contrasena incorrectos");
+    }
+}
+function CrearCuenta(){
+    let v1 = false;
+    let v2 = false;
+    let v3 = false;
+    if (usuarioCrearCuenta.value.trim() !== "") {
+        cuenta.NombreUsuario = usuarioCrearCuenta.value;
+        v1 = true;
+    }
+    if (contrasenaCrearCuenta.value.trim() !== "") {
+        cuenta.Contrasena = contrasenaCrearCuenta.value;
+        v2 = true;
+    }
+    if (emailCrearCuenta.value.trim() !== "") {
+        cuenta.Email = emailCrearCuenta.value;
+        v3 = true;
+    }
+    if(!(v1 && v2 && v3)){
+        alert("Por favor complete todos los campos");
+        return;
+    }
+    else{
+        alert("Cuenta creada exitosamente");
+    }
+
+    crearCuenta.style.display = "none";
+    iniciarSesion.style.display = "block";
 }
